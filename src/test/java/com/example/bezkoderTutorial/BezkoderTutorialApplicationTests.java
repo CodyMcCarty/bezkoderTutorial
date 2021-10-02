26d64e10e42013a1bb945975ef7dbeaa8a811198
“Cody <cmccarty@catalyte.io>
2021-10-02 16:37:17 -0500

    initial commit

---
+++ src/test/java/com/example/bezkoderTutorial/BezkoderTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bezkoderTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezkoderTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
